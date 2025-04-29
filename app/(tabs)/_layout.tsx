dabd171a2fe068571353eae2985508ab46297f25
aleksanderkrys <aleksander.krys@lightcode.com.pl>
2025-04-29 19:22:39 +0200

    bottom tab nav adjusted

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,63 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import  HomeIcon from '@/assets/images/icons/home-icon.svg';
+import SearchIcon from '@/assets/images/icons/search-icon.svg';
+import { useFonts } from 'expo-font';
+import { Poppins_700Bold } from '@expo-google-fonts/poppins';
+import { Poppins_400Regular } from '@expo-google-fonts/poppins';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+  const [fontsLoaded] = useFonts({
+    'Poppins-Regular': Poppins_400Regular,
+    'Poppins-Bold': Poppins_700Bold,
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: '#2B2D42',
+        tabBarInactiveTintColor: '#FFFFFF',
         headerShown: false,
         tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
+        tabBarLabelStyle:{
+          fontSize:16,
+          fontFamily:'Poppins-Regular',
+    
+        },
+        tabBarItemStyle: {
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          paddingVertical: 8, // Add padding for better spacing
+        },
+    
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
-          default: {},
+          default: {
+            backgroundColor:'#8D99AE',
+            height:72,
+          },
         }),
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ focused }) => <HomeIcon width={28} height={28} fill={focused ? '#2B2D42' : '#FFFFFF'} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="search"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Search',
+          tabBarIcon: ({ focused }) => <SearchIcon width={28} height={28} stroke={focused ? '#2B2D42' : '#FFFFFF'} />,
         }}
       />
     </Tabs>
